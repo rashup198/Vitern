948ae0d79f006ae1b03de383793a2913e3eb6f90
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-28 18:35:15 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 import "./index.css"
+
 
 function App() {
   return (
-    <div className="App text-5xl font-bold">
-      Hello jiii
-    </div>
-  );
+   <div className='h-[100vh] w-[100vw] bg-[#e4eff3]'>
+    <Navbar></Navbar>
+   </div> 
+  )
 }
 
 export default App;
