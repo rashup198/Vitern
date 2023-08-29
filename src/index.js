22bc6b1dd8796cbea22b5357ff96f85799167854
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-29 23:20:57 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Auth0Provider } from '@auth0/auth0-react'
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
-import App from './App';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
-
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div className=''>
-    <Auth0Provider
+  <BrowserRouter>
+  <Auth0Provider
     domain="dev-4p1c8ufdtgrvebgr.us.auth0.com"
     clientId="bCp2by4O0uBJXdlPG0opaPpQlbHxm37x"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
   >
-    <App />
+       <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About/>} />
+
+      </Routes>
   </Auth0Provider>
-  </div>
-  
+  </BrowserRouter>
 );
