28b5ca0a39adddc1ce557ed78512cc9ee1cdc12a
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-27 01:21:24 +0530

    Add VerifyEmail component and related functionality

---
+++ frontend/src/services/apis.js
@@ -0,0 +1,10 @@
+
+// AUTH ENDPOINTS
+export const endpoints = {
+  SENDOTP_API: "http://localhost:5000/api/v1/auth/sendotp",
+  SIGNUP_API: "http://localhost:5000/api/v1/auth/signup",
+  LOGIN_API:  "http://localhost:5000/api/v1/auth/login",
+  RESETPASSTOKEN_API:"http://localhost:5000/api/v1/auth/reset-password-token",
+  RESETPASSWORD_API: "http://localhost:5000/api/v1/auth/reset-password",
+}
+
