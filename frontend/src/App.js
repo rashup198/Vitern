b717ff4e085c3fa52d7264c87f8a18c367d2ee8d
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-11-27 01:20:34 +0530

    Refactor App.js file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,74 @@
-import './App.css';
-import About from './Components/About';
-import Navbar from './Components/Navbar';
-import "./index.css"
+import { useEffect } from "react"
+import "./App.css"
+// Redux
+import { useDispatch, useSelector } from "react-redux"
+// React Router
+import { Route, Routes, useNavigate } from "react-router-dom"
+import Navbar from "./Components/Common/Navbar"
+import OpenRoute from "./Components/core/Auth/OpenRoute"
+import About from "./Components/About"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import UpdatePassword from "./pages/UpdatePassword"
+import VerifyEmail from "./pages/VerifyEmail"
+import ForgotPassword from "./pages/ForgotPassword"
 
 function App() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { token } = useSelector((state) => state.auth)
+
+
   return (
-    <div className='h-[100vh] w-[100vw] bg-[#e4eff3]'>
- 
-    </div> 
-  );
+    <>
+    <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
+      <Navbar/>
+      <Routes>
+        <Route path="/about" element={<About></About>}/>
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+        
+        </Routes>
+    </div>
+    </>
+  )
 }
-export default App;
+export default App
